6aee5e887b9ec9badee5def5865d7c47306fd6d0
Ezaz <ezaznahmed@gmail.com>
2022-02-13 14:47:44 +0600

    Tow Sum done

--- twoSum/twoSum.go
+++ twoSum/twoSum.go
@@ -3,6 +3,20 @@ package main
 import "fmt"
 
 func main() {
+	nums := []int{2, 7, 11, 15}
+	target := 9
 
-	fmt.Println("Two Sum")
+	indexMap := make(map[int]int)
+
+	for currIndex, currentNumber := range nums {
+
+		if value, ok := indexMap[target-currentNumber]; ok {
+			fmt.Println("[", value, currIndex, "]")
+		} else {
+			indexMap[currentNumber] = currIndex
+		}
+
+	}
+	fmt.Println("[]")
+
 }
