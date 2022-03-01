2a0ea688809c653ed438d265e19f40b24c47892c
Ezaz <ezaznahmed@gmail.com>
2022-03-01 16:34:50 +0600

    commit

---
+++ summaryRanges/summaryRanges.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+
+	// nums := []int{0, 1, 2, 4, 5, 7}
+	nums := []int {0,1,2,3,4,6,7,8,10,11,12,14}
+	// nums := []int {0,2,3,4,6,8,9}
+
+	for i := 0; i < len(nums); i++ {
+		if nums[i] + 1 =
+	}
+}
