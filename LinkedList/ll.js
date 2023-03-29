f34691fac0841f8d6c7bc2a7d5d88f70d1dc6e82
Ezaz Ahmed <ezaznahmed@gmail.com>
2023-03-29 16:31:59 +0600

    Linked List

--- LinkedList/ll.js
+++ LinkedList/ll.js
@@ -20,11 +20,57 @@ class LinkedList {
       temp = temp.next;
     }
   }
+
+  getHead() {
+    if (this.head === null) {
+      console.log('Head: null');
+    } else {
+      console.log(`Head: ${this.head.value}`);
+    }
+  }
+
+  getTail() {
+    if (this.tail === null) {
+      console.log('Tail: null');
+    } else {
+      console.log(`Tail: ${this.tail.value}`);
+    }
+  }
+
+  getLength() {
+    console.log('Length: ' + this.length);
+  }
+
+  makeEmpty() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(value) {
+    const node = new Node(value);
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    }
+
+    this.tail.next = node;
+    this.tail = node;
+    this.length++;
+
+    return this;
+  }
 }
 
 const test = () => {
-  const ll = new LinkedList(1);
+  let ll = new LinkedList(1);
+  ll.push(2);
+  ll.push(3);
+  ll.push(4);
+
   ll.printLinkedList();
+
+  ll.getLength();
 };
 
 test();
