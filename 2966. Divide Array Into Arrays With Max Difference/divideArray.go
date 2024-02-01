045a6e5354e89aaaf1cec07595262c17fb3d3e74
Ezaz Ahmed <ezaznahmed@gmail.com>
2024-02-01 22:10:06 +0600

    solve: leetcode 2966

---
+++ 2966. Divide Array Into Arrays With Max Difference/divideArray.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"slices"
+)
+
+func divideArray(nums []int, k int) [][]int {
+	slices.Sort(nums)
+	results := [][]int{}
+
+	for i := 0; i < len(nums); i = i + 3 {
+		if nums[i+2]-nums[i] > k {
+			return [][]int{}
+		} else {
+			results = append(results, nums[i:i+3])
+		}
+	}
+
+	return results
+}
+
+func main() {
+	// divideArray([]int{1, 3, 4, 8, 7, 9, 3, 5, 1}, 2)
+	fmt.Println(divideArray([]int{1, 3, 4, 8, 7, 9, 3, 5, 1}, 2))
+	fmt.Println(divideArray([]int{1, 3, 3, 2, 7, 3}, 3))
+}
